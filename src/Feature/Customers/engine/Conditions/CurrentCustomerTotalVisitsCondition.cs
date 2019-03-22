dd90e75d41859cab49fe8ac115723cdc8c1196ef
Kazim Nami <kna@sitecore.net>
2019-03-22 16:58:48 +1100

    Added shopper behaviour promotion qualifications

---
+++ src/Feature/Customers/engine/Conditions/CurrentCustomerTotalVisitsCondition.cs
@@ -0,0 +1,29 @@
+﻿using Sitecore.Commerce.Core;
+using Sitecore.Commerce.Plugin.Carts;
+using Sitecore.Commerce.Plugin.Customers;
+using Sitecore.Framework.Rules;
+using System.Linq;
+
+namespace Feature.Customers.Engine
+{
+    [EntityIdentifier(CustomersConstants.Conditions.CurrentCustomerTotalVisitsConditionName)]
+    public class CurrentCustomerTotalVisitsCondition : ICustomerCondition, ICondition, IMappableRuleEntity
+    {
+        public IRuleValue<int> TotalVisits { get; set; }
+        public IBinaryOperator<int, int> Operator { get; set; }
+
+        public bool Evaluate(IRuleExecutionContext context)
+        {
+            if (context == null || Operator == null)
+                return false;
+
+            var targetTotalVisits = TotalVisits?.Yield(context);
+            var cart = context.Fact<CommerceContext>()?.GetObject<Cart>();
+            if (cart == null || !cart.Lines.Any() || targetTotalVisits != null)
+                return false;
+
+            var component = cart.GetComponent<CartContactBehaviourComponent>();
+            return Operator.Evaluate(component.TotalVisits, targetTotalVisits.Value);
+        }
+    }
+}
